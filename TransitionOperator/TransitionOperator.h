844eec85c2b906629bf5ca22e8851c52201517a5
Ryoichi Izumita <r.izumita@caph.jp>
2016-02-23 17:50:24 +0900

    Initial Commit

---
+++ TransitionOperator/TransitionOperator.h
@@ -0,0 +1,19 @@
+//
+//  TransitionOperator.h
+//  TransitionOperator
+//
+//  Created by 和泉田 領一 on 2016/02/23.
+//  Copyright © 2016年 CAPH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TransitionOperator.
+FOUNDATION_EXPORT double TransitionOperatorVersionNumber;
+
+//! Project version string for TransitionOperator.
+FOUNDATION_EXPORT const unsigned char TransitionOperatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TransitionOperator/PublicHeader.h>
+
+
---
+++ TransitionOperatorTests/TransitionOperatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  TransitionOperatorTests.swift
+//  TransitionOperatorTests
+//
+//  Created by 和泉田 領一 on 2016/02/23.
+//  Copyright © 2016年 CAPH. All rights reserved.
+//
+
+import XCTest
+@testable import TransitionOperator
+
+class TransitionOperatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
